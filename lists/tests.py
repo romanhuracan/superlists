51a5f103bd620afa7447c53fe9db92b975d0ce08
Roman <khabirov_rr@bk.ru>
2021-02-26 12:20:25 +0500

    Обработка первого POST-запроса и вывод данных в таблицу

--- lists/tests.py
+++ lists/tests.py
@@ -13,3 +13,9 @@ class HomePageTest(TestCase):
         """ тест: домашняя страница возвращает правильный HTML """
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST_request(self):
+        """ тест: можно сохранить POST-запрос """
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, "home.html")
