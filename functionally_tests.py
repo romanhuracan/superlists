ae6af0d404776f26c957ed40765198995198686c
Roman <khabirov_rr@bk.ru>
2021-02-26 12:26:41 +0500

    Рефакторизация дублирования кода

--- functionally_tests.py
+++ functionally_tests.py
@@ -14,6 +14,12 @@ class NewVisitorTest(unittest.TestCase):
         """ Демонтаж """
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """ подтверждение строки в таблице списка """
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """ тест: можно начать список и получть его позже """
         # Эдит слышала про крутое новое онлайн-приложение со списком
@@ -41,10 +47,7 @@ class NewVisitorTest(unittest.TestCase):
         # содержит ""1: Купить павлиньи перья" в качестве элемента сиска
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушке из павлиньих перьев"
@@ -55,10 +58,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Купить павлиньи перья", [row.text for row in rows])
-        self.assertIn("2: Сделать мушку из павлиньих перьев", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Купить павлиньи перья")
+        self.check_for_row_in_list_table("2: Сделать мушку из павлиньих перьев")
 
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес - об этом
