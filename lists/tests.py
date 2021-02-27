3e3f6803cbd805ee3eb25f3bb31d39ab270124ad
Roman <khabirov_rr@bk.ru>
2021-02-27 17:34:59 +0500

    Новый URL + представление для добавления в существующие списки. ФТ проходит :)

--- lists/tests.py
+++ lists/tests.py
@@ -12,19 +12,33 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         """ тест: используется шаблон списка """
-        response = self.client.get("/lists/unicum-list/")
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
         self.assertTemplateUsed(response, "list.html")
 
-    def test_displays_all_list_items(self):
-        """ тест: отображаются все элементы списка """
-        list_ = List.objects.create()
-        Item.objects.create(text="itemey 1", list=list_)
-        Item.objects.create(text="itemey 2", list=list_)
+    def test_displays_only_items_for_that_list(self):
+        """ тест: отображаются элементы только для этого списка """
+        correct_list = List.objects.create()
+        Item.objects.create(text="itemey 1", list=correct_list)
+        Item.objects.create(text="itemey 2", list=correct_list)
 
-        response = self.client.get("/lists/unicum-list/")
+        other_list = List.objects.create()
+        Item.objects.create(text="другой элемент 1 списка", list=other_list)
+        Item.objects.create(text="другой элемент 2 списка", list=other_list)
+
+        response = self.client.get(f"/lists/{correct_list.id}/")
 
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
+        self.assertNotContains(response, "другой элемент 1 списка")
+        self.assertNotContains(response, "другой элемент 2 списка")
+
+    def test_passes_correct_list_to_template(self):
+        """ тест: передается правильный шаблон списка """
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f"/lists/{correct_list.id}/")
+        self.assertEqual(response.context["list"], correct_list)
 
 
 class NewListTest(TestCase):
@@ -40,7 +54,39 @@ class NewListTest(TestCase):
     def test_redirect_after_POST(self):
         """ тест: переадресует после POST-запроса """
         response = self.client.post("/lists/new", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/lists/unicum-list/")
+        new_list = List.objects.first()
+        self.assertRedirects(response, f"/lists/{new_list.id}/")
+
+
+class NewItemTest(TestCase):
+    """ тест нового элемента списка """
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        """ тест: можно сохранить POST-запрос в существующий список """
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for an existing list"}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new item for an existing list")
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        """ тест: переадресуется в представление списка """
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "A new item for an existing list"}
+        )
+
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
 
 class HomePageTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,11 @@ from django.http import HttpResponse
 from lists.models import Item, List
 
 
-def view_list(request):
+def view_list(request, list_id):
     """ представление списка """
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, "list.html", {"list": list_})
 
 
 def home_page(request):
@@ -19,4 +20,11 @@ def new_list(request):
     """ новый список """
     list_ = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/unicum-list/")
+    return redirect(f"/lists/{list_.id}/")
+
+
+def add_item(request, list_id):
+    """ добавить элемент """
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
