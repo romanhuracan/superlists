78657201cb058d41c8d3eec87d27804eedef2934
Roman <khabirov_rr@bk.ru>
2021-02-28 12:40:09 +0500

    Использован Bootstrap для улучшения макета

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,5 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,7 +11,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """ Тесто нового посетителя """
     def setUp(self):
         """ Установка """
